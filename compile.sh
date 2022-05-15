23a11f8dbf8e772d15848cc2f2df90826ab685c6
Brian Zick <brian@zick.io>
2022-05-15 16:58:57 -0500

    include fonts and sshconfig in priv

--- compile.sh
+++ compile.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 tar -cv @spec/base.mtree > build/base.tar
+
+command -v git >/dev/null 2>&1 || { echo Git required to build priv; exit; }
+
+git submodule init
+git submodule update
+
+tar -cv @spec/priv.mtree > build/priv.tar
