b7af45ab8b63401e7a8b26911b8cce0da6ca7914
Brian Zick <brian@zick.io>
2022-06-01 09:52:09 -0500

    update xbar script for kexp

--- macos/xbar/current_song.15s.sh
+++ macos/xbar/current_song.15s.sh
@@ -2,7 +2,7 @@
 
 SONG=`/usr/local/bin/mpc --host=gh -f "[%title%[ - %artist%]]|[%name%]|[%file%]"|head -n1|sed 's/^[ \-]*//;s/[ \-]*$//'|grep -v "^volume: "`
 
-if echo $SONG | grep -q 'KEXP'; then
+if echo $SONG | grep -Eq 'KEXP|kexp128'; then
 	SONG=`curl 'https://api.kexp.org/v2/plays/?format=json&limit=1' -s -H 'User-Agent: Mozilla/5.0 (X11; OpenBSD amd64; rv:72.0) Gecko/20100101 Firefox/72.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' | \
 	/usr/local/bin/jq -j '.results[].song, " - ", .results[].artist' | \
 	/usr/bin/sed 's/null\ \-\ null//'` 
